c7b3fe44681c30c2b8e0e76fb4c81c72aaba639c
Florian Wyss <fw@studer-raimann.ch>
2018-11-30 08:40:05 +0100

    PHPVersionChecker

--- plugin.php
+++ plugin.php
@@ -1,4 +1,7 @@
 <?php
+
+require_once __DIR__ . "/../../../Repository/RepositoryObject/H5P/vendor/srag/dic/src/PHPVersionChecker.php";
+
 $id = "pchfp";
 $version = "1.1.0";
 $ilias_min_version = "5.2.0";
