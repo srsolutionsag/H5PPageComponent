96a34f82aea93ab6b63d8e2ee03ab2faa3c7845f
Florian Wyss <fw@studer-raimann.ch>
2020-05-28 13:35:09 +0200

    Fix

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../../../Repository/RepositoryObject/H5P/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "pchfp";
-$version = "1.3.5";
+$version = "1.4.0";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "6.999";
 $responsible = "studer + raimann ag - Team Custom 1";
