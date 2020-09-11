1d5422885408cfe93d7a43f68b321a44d630f499
Florian Wyss <fw@studer-raimann.ch>
2020-09-11 13:42:17 +0200

    Fixes

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = "pchfp";
-$version = "1.4.0";
+$version = "1.4.1";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "6.999";
 $responsible = "studer + raimann ag - Team Custom 1";
