68c60b03cb3c9abd901b3f730f8627a3c4512ef9
Thibeau Fuhrer <thf@studer-raimann.ch>
2021-11-17 10:18:33 +0100

    increased plugin-version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = "pchfp";
-$version = "1.5.3";
+$version = "1.5.4";
 $ilias_min_version = "5.4.0";
 $ilias_max_version = "6.999";
 $responsible = "studer + raimann ag - Team Custom 1";
